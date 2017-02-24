be1226770ec0f6c94be68015499b161b142929b6
internousdev <internousdev@00196286477657>
2017-02-24 15:25:24 +0900

    席替えのため

---
+++ src/com/internousdev/struts2/dao/LoginDAO.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.internousdev.struts2.dao;
+
+/**
+ * @author internousdev
+ *
+ */
+public class LoginDAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/com/internousdev/struts2/dto/LoginDTO.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.internousdev.struts2.dto;
+
+/**
+ * @author internousdev
+ *
+ */
+public class LoginDTO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
